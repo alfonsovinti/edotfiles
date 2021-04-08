f93c56fe23b3c75bddf10606077d119358af08ea
alfonsovinti <me@alfonsovinti.it>
2021-04-08 13:29:38 +0200

    add

---
+++ early-init.el
@@ -0,0 +1,7 @@
+;;; early-init.el - Emacs 27+ pre-initialisation config
+
+;; Emacs 27+ loads this file before (normally) calling
+;; `package-initialize'.  We use this file to suppress that automatic
+;; behaviour so that startup is consistent across Emacs versions.
+
+(setq package-enable-at-startup nil)

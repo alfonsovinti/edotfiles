da92aa700ea7b6d6cfad1ae1f6a7955ef021fdc3
alfonsovinti <me@alfonsovinti.it>
2021-04-16 12:17:04 +0200

    edit

--- elisp/edf-keymap.el
+++ elisp/edf-keymap.el
@@ -23,4 +23,28 @@
   "tc"   '(rainbow-mode :which-key "rainbow mode")
   "tt"   '(counsel-load-theme :which-key "choose theme"))
 
+;; KEYS
+;; "*"    Search for symbol in project
+;; "."    Find file
+;; "/"    Seach in project
+;;        Switc to last buffer
+;; "b"    '(:ignore t :which-key "buffer")
+;; "c"    '(:ignore t :which-key "code")
+;; "f"    '(:ignore t :which-key "file")
+;; "g"    '(:ignore t :which-key "git")
+;; "h"    '(:ignore t :which-key "help")
+;; "i"    '(:ignore t :which-key "insert")
+;; "o"    '(:ignore t :which-key "open")
+;; "m"    '(:ignore t :which-key "bookmarks")
+;; "n"    '(:ignore t :which-key "notes")
+;; "p"    '(:ignore t :which-key "project")
+;; "q"    '(:ignore t :which-key "quit/session")
+;; "s"    '(:ignore t :which-key "search")
+;; "t"    '(:ignore t :which-key "toggles")
+;; "u"    '(:ignore t :which-key "ui")
+;; "w"    '(:ignore t :which-key "window")
+;; "x"    '(:ignore t :which-key "execute")
+
+
+
 (provide 'edf-keymap)

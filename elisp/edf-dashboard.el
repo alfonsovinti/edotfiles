c8d0683fc381540785383c5dd8ee8509b3ff4881
alfonsovinti <me@alfonsovinti.it>
2021-04-19 14:17:29 +0200

    customize dashboard

--- elisp/edf-dashboard.el
+++ elisp/edf-dashboard.el
@@ -2,6 +2,28 @@
   :config
   (dashboard-setup-startup-hook)
   (setq dashboard-set-heading-icons t)
-  (setq dashboard-set-file-icons t))
+  (setq dashboard-set-file-icons t)
+  (setq dashboard-startup-banner 2)
+  ;(setq dashboard-banner-logo-title "Emacs Is More Than A Text Editor!")
+  (setq dashboard-center-content t)
+;  (setq dashboard-items '((recents . 5)
+;                          (agenda . 5 )
+;                          (registers . 3)))
+  (setq dashboard-items '((recents  . 5)
+                          (agenda . 5)
+                          (bookmarks . 5)
+                          ;(projects . 3)
+                          (registers . 5)))
+;  (dashboard-modify-heading-icons '((recents . "file-text")
+;                                  (bookmarks . "book")))
+  (setq dashboard-set-navigator t)
+  (setq dashboard-navigator-buttons
+    ;; Format: "(icon title help action face prefix suffix)"
+    `((("" "" "" nil nil "" "")) ;; line1
+        ((,(all-the-icons-octicon "mark-github" :height 1.1 :v-adjust 0.0)
+            "Homepage"
+            "Browse homepage"
+            (lambda (&rest _) (browse-url "https://github.com/alfonsovinti/edotfiles"))))))
+  (setq dashboard-set-footer nil))
 
 (provide 'edf-dashboard)

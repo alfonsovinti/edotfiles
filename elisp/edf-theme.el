28f9cf29bf728d44add36cd19046dd86417f101b
alfonsovinti <me@alfonsovinti.it>
2021-04-21 12:03:23 +0200

    add neotree

--- elisp/edf-theme.el
+++ elisp/edf-theme.el
@@ -2,6 +2,8 @@
 ;; installing this package!
 (use-package all-the-icons)
 
+(use-package neotree)
+
 ;(use-package nord-theme
 ;  :init
 ;    ;; Use `nord4` from Nord's "Snow Storm" palette as background color.
@@ -14,17 +16,19 @@
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
   (load-theme 'doom-nord t)
+  ;; Configure neotree theme
+  (setq doom-themes-neotree-file-icons t)
+  (doom-themes-neotree-config)
   ;; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)
- )
+  (doom-themes-visual-bell-config))
 
 (use-package minions
   :hook (doom-modeline-mode . minions-mode))
 
 (use-package doom-modeline
 ;  :after eshell ;; make sure it gets hooked after eshell
-  :init (doom-modeline-mode 1)
 ;  :hook (after-init . doom-modeline-init)
+  :init (doom-modeline-mode 1)
   :custom-face
   (mode-line ((t (:height 0.95))))
   (mode-line-inactive ((t (:height 0.95))))
@@ -34,10 +38,9 @@
   (doom-modeline-minor-modes t)
   (doom-modeline-indent-info t)
 ;  (doom-modeline-lsp t)
-  (doom-modeline-irc nil)
 ;  (doom-modeline-persp-name nil)
 ;  (doom-modeline-buffer-file-name-style 'truncate-except-project)
 ; (doom-modeline-major-mode-icon nil))
-)
+  (doom-modeline-irc nil))
 
 (provide 'edf-theme)
